155f341612ccc653e670a2a30f92fcc858f5ba75
cdsalty <soltiscd@gmail.com>
2020-08-14 00:18:53 -0400

    working on contries being undefiend

--- MasteryMain/currencyConverter_asyncAwait/app.js
+++ MasteryMain/currencyConverter_asyncAwait/app.js
@@ -1,4 +1,5 @@
-import axios from "axios";
+// import axios from "axios";
+const axios = require("axios");
 
 const FIXER_API_KEY = "a8430199fe48e4147ccb420fe9376a8e"; // https://www.oanda.com/foreign-exchange-data-services/en/exchange-rates-api/
 const FIXER_API = `http://data.fixer.io/api/latest?access_key=${FIXER_API_KEY}`; // fixer.io
@@ -9,12 +10,12 @@ const getExchangeRate = async (fromCurrency, toCurrency) => {
 	// const response = await axios.get(FIXER_API); ---> destructured out data
 	const {data} = await axios.get(FIXER_API);
 	// console.log(data.rates);
-	const eruo = 1 / data.rates[fromCurrency];
+	const eruo = 1 / data.rates[fromCurrency]; //?
 	const exchangeRate = eruo * data.rates[toCurrency];
 
 	return exchangeRate; // will need a ".then"?
 };
-console.log(getExchangeRate("USD", "EUR"));
+// console.log(getExchangeRate("USD", "EUR"));// WILL RETURN A PROMISE //?
 
 // Fetch Data about countries (example: which countries can a currency be used in?)
 const getCountries = async currencyCode => {
@@ -25,7 +26,7 @@ const getCountries = async currencyCode => {
 	// console.log(data)
 };
 
-console.log(getCountries("AUD"));
+// console.log(getCountries("AUD"));
 
 // Gather data and convert currency
 const convertCurrency = async (fromCurrency, toCurrency, amount) => {
@@ -33,14 +34,23 @@ const convertCurrency = async (fromCurrency, toCurrency, amount) => {
 	fromCurrency = fromCurrency.toUpperCase();
 	toCurrency = toCurrency.toUpperCase();
 
-	// Implementing PROMISE.ALL - see destructured example in notes
+	// Implementing PROMISE.ALL - see destructured example in notes (const values = await Promise.all--- I pulled out exchangRate and countries from values)
 	const [exchangeRate, countries] = await Promise.all([
 		// now we can pull the values at the same time instead of waiting for each function to run.
-		getExchangeRate(fromCurrency, toCurrency),
-		getCountries(toCurrency)
+		await getExchangeRate(fromCurrency, toCurrency),
+		await getCountries(toCurrency)
 	]);
+	console.log(countries);
+	const convertedAmount = (amount * exchangeRate).toFixed(2);
+
+	return `${amount} ${fromCurrency} is worth ${convertedAmount} ${toCurrency}. You can spend these in the following: ${countries}`;
+	// console.log(countries);
+	// console.log(exchangeRate);
 };
 
+convertCurrency("USD", "HRK", 20)
+	.then(result => console.log(result))
+	.catch(error => console.log(error));
 /*
 data.map((country) => country.name)
 destructured: 
